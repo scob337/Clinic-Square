fcba975fd8a8a191b08780deeddee465e442adb7
Abd Eltawab Shaaban <scob198350@gmail.com>
2024-09-05 06:35:22 +0300

    Handle Doctor card component

---
+++ src/Components/DoctorCard/Doctors_Card.jsx
@@ -0,0 +1,28 @@
+import { FaStar } from "react-icons/fa";
+
+export default function Doctors_Card({Item , Button}) {
+
+    const {name, speciality, image, rank } = Item;
+  return (
+    <div className="max-sm:w-[100%] md:w-[300px]  lg:w-[250px] h-[300px] 
+    rounded-lg
+    flex flex-col justify-center items-center gap-2 ring-1 ring-[#00ACA8]" > 
+
+      <div className="flex flex-col items-center relative gap-2
+       before:content-[''] before:absolute before:top-9 before:left-0 before:w-[100%] before:h-[50%] before:bg-[#00ACA8] before:opacity-60  before:rounded-lg
+      
+      ">
+      <img src={image} alt="Doctor" className="w-[70%] h-[150px] object-cover  z-[9999]
+      
+      " />
+      <h2 className="text-center text-[14px] font-bold text-[#404040]">{name}</h2>
+      <p className="text-center text-[12px] text-[#767676]">{speciality}</p>
+      <p className="text-center text-[12px] flex items-center gap-2"> <FaStar color='#F8A019'
+      size={18}/> 
+      <span className="text-[#000] font-semibold" > ({rank})</span></p>
+      </div>
+
+      {Button}
+    </div>
+  )
+}
--- src/Components/DoctorCard/MainCard.jsx
+++ src/Components/DoctorCard/MainCard.jsx
@@ -1,5 +1,5 @@
+import Doctors_Card from './Doctors_Card';
 import DocData from './DoctorsData'
-import { FaStar } from "react-icons/fa";
 
 export default function MainCard() {
   
@@ -8,31 +8,14 @@ export default function MainCard() {
 
       {DocData.map((item) => {
         return (
-          <div className="max-sm:w-[100%] md:w-[300px]  lg:w-[250px] h-[300px] 
-          rounded-lg
-          flex flex-col justify-center items-center gap-2 ring-1 ring-[#00ACA8]" key={item.id}> 
-
-            <div className="flex flex-col items-center relative gap-2
-             before:content-[''] before:absolute before:top-9 before:left-0 before:w-[100%] before:h-[50%] before:bg-[#00ACA8] before:opacity-60  before:rounded-lg
-            
-            ">
-            <img src={item.image} alt="Doctor" className="w-[70%] h-[150px] object-cover  z-[9999]
-            
-            " />
-            <h2 className="text-center text-[14px] font-bold text-[#404040]">{item.name}</h2>
-            <p className="text-center text-[12px] text-[#767676]">{item.speciality}</p>
-            <p className="text-center text-[12px] flex items-center gap-2"><FaStar color='#F8A019'
-            size={18}/> 
-            <span className="text-[#000] font-semibold" > ({item.rank})</span></p>
-            </div>
-            <button className='ring-1 ring-[#00ACA8] p-2 w-[70%] rounded-2xl text-[13px]
+          <Doctors_Card key={item.id} Item={item} Button={      <button className='ring-1 ring-[#00ACA8] p-2 w-[70%] rounded-2xl text-[13px]
             text-[#00ACA8] font-semibold
             transition duration-200 hover:bg-[#00ACA8] hover:text-white
-            '>Book an Appointment</button>
-          </div>
+            '>Book an Appointment</button>}/>
+          
         )
       })}
-
+  
     </div>
   )
 }
